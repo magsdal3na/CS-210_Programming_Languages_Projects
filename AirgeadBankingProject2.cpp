2f4903dbd61227945b97c3131d49cd2a00d1c3bf
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-03-26 13:16:04 -0500

    Update AirgeadBankingProject2.cpp
    added data input and updated screen. committing before changing the formatting on output w/ values

--- AirgeadBankingProject2.cpp
+++ AirgeadBankingProject2.cpp
@@ -18,9 +18,42 @@ void WelcomeScreen(int& selection) { //sets up the welcome screen
 	cin >> selection;
 }
 
-void DataInput() {
+void DataInputScreen() { //setting up basic data input screen for user
 	cout << setfill('*') << setw(34) << "" << endl;
-	cout << setfill('*') << setw(10) << "" << " Data Input " << setw(10) << "" << endl;
+	cout << setfill('*') << setw(11) << "" << " Data Input " << setw(11) << "" << endl;
+	cout << setfill(' ') << setw(34) << left << "Initial Investment Amount: " << endl;
+	cout << setw(34) << left << "Monthly Deposit: " << endl;
+	cout << setw(34) << left << "Annual Interest: " << endl;
+	cout << setw(34) << left << "Number of Years: " << endl;
+	cout << setw(34) << left << "Press 1 to continue. . ." << endl;
+}
+
+void DataInput() {
+	double investment;
+	double monthlyDeposit;
+	double annualInterest;
+	int numOfYears;
+
+	cout << "Please enter the starting amount of your investment: " << endl;
+	cout << ">>";
+	cin >> investment;
+	cout << "Please enter your monthly deposit amount: " << endl;
+	cout << ">>";
+	cin >> monthlyDeposit;
+	cout << "Please enter the annual interest amount: " << endl;
+	cout << ">>";
+	cin >> annualInterest;
+	cout << "Please enter the number of years for the investment: " << endl;
+	cout << ">>";
+	cin >> numOfYears;
+
+	cout << setfill('*') << setw(34) << "" << endl;
+	cout << setfill('*') << setw(11) << "" << " Data Input " << setw(11) << "" << endl;
+	cout << setfill(' ') << setw(31) << left << "Initial Investment Amount:  " << "$" << investment << endl;
+	cout << setw(31) << left << "Monthly Deposit:  " << "$" << monthlyDeposit << endl;
+	cout << setw(31) << left << "Annual Interest:  " << annualInterest << "%" << endl;
+	cout << setw(31) << left << "Number of Years:  " << numOfYears << endl;
+	cout << setw(34) << left << "Press 1 to continue. . ." << endl;
 }
 
 int main() {
@@ -29,7 +62,7 @@ int main() {
 	WelcomeScreen(selection);
 
 	while (selection == 1) {
-		Test();
+		DataInputScreen();
 		DataInput();
 		break;
 	}
