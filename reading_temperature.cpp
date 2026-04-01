1a7d62ee4d064c7f32e7fd7961e073ed55220ee5
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-01 13:21:43 -0500

    Update reading_temperature.cpp
    got file to work. created output file CelsiusTemperature.txt.

--- reading_temperature.cpp
+++ reading_temperature.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip> //for formatting decimals
 using namespace std;
 
 int main() {
 	ifstream inFS;	//input file stream
+	ofstream outFS; //output file stream
 	string city;
 	int tempFahrenheit;
+	double tempCelsius; //double to prevent 0 degree answer
 
-	//file opening attempt
+	//file opening attempt for input
 	cout << "Opening file: FahrenheitTemperature.txt" << endl;
 
 	inFS.open("FahrenheitTemperature.txt");
 	if (!inFS.is_open()) {
-		cout << "Could not open file: FahrenheitTemperature.txt" << endl;
+		cout << "Could not open file: FahrenheitTemperature.txt." << endl;
 		return 1; //error
+	}
+
+	//opening attempt for output
+	outFS.open("CelsiusTemperature.txt");
+	if (!outFS.is_open()) {
+		cout << "Could not open file CelsiusTemperature.txt." << endl;
+		return 1;
 	}
 
 	//reading and printing input to output
@@ -26,8 +36,16 @@ int main() {
 		}
 	}
 
-	//closing file once read
+	while (inFS >> city >> tempFahrenheit) {
+		//perform conversion
+		tempCelsius = (tempFahrenheit - 32.0) * (5.0 / 9.0);
+
+		//write to new file
+		outFS << city << " " << fixed << setprecision(2) << tempCelsius << endl;
+	}
+	//closing both files once read
 	inFS.close();
+	outFS.close();
 
 	return 0;
 }
