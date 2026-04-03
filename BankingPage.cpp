5759588d380ac64ab3c3889e18728b6336abb40e
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-03 15:06:51 -0500

    Update BankingPage.cpp
    added formatting and more user friendly details

--- BankingPage.cpp
+++ BankingPage.cpp
@@ -7,15 +7,18 @@ int main() {
 	Investment myAccount; //constructor
 	int selection = 0;
 
-	cout << "Start = 1, Quit = 0" << endl;
+	cout << setfill('*') << setw(36) << "" << endl;
+	cout << setfill('*') << setw(13) << "" << " Welcome! " << setw(13) << "" << endl;
+	cout << "To start the program, please enter 1." << endl << "To quit, please enter 2." << endl;
+	cout << ">>";
 	cin >> selection;
 
 	while (selection == 1) {
 		myAccount.promptForData();
-		myAccount.displayWithDeposits();
 		myAccount.displayWithoutDeposits();
-
-		cout << "Continue?" << endl;
+		myAccount.displayWithDeposits();
+		
+		cout << "Press 1 to continue or 2 to quit." << endl;
 		cin >> selection;
 	}
 	return 0;
